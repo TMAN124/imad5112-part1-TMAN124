6db24583629966f1ddd549874b1418776d3733b1
TSHEPO <ST10449338@imconnect.edu.za>
2024-04-05 17:54:55 +0200

    part 1

---
+++ app/src/main/java/com/example/brazilianb001ylift/MainActivity.kt
@@ -0,0 +1,80 @@
+package com.example.brazilianb001ylift
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val generateHistoryButton = findViewById<Button>(R.id.searchButton)
+
+        val editText = findViewById<EditText>(R.id.editTextNumber)
+
+        val textView = findViewById<EditText>(R.id.editTextNumber)
+
+
+        generateHistoryButton?.setOnClickListener {
+            val inputText = editText.text.toString()
+
+
+            if (inputText.isNotEmpty() && inputText.all { it.isDigit() }) {
+
+                var inputNumber = inputText.toInt()
+
+
+
+
+                if (inputNumber >= 20 && inputNumber < 100) {
+
+                    if (inputNumber == 42) {
+
+                        textView.text = "Elvis Presley died at the age of 42"
+
+                    } else if (inputNumber == 39) {
+
+                        textView.text = "Cleopatra died at 39"
+
+                    }
+
+
+                    if (inputNumber == 40) {
+
+                        textView.text = "Paul Walker died at 40"
+
+                    } else if (inputNumber == 52) {
+
+                        textView.text =
+
+                            "Wiliam Shakespeare died at 52"
+                    }
+
+                }
+
+
+
+                if (inputNumber == 56) {
+
+                    textView.text =
+                        "Abraham Lincoln died at 56"
+
+                } else if (inputNumber == 78) {
+
+                    textView.text =
+
+                        "Mahatma Gandi died at 78"
+
+
+                }
+
+            }else {textView.text= "your entry was not valid"
+
+
+        }
+
+
+    }
+}}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "brazilian B001y Lift"
+include(":app")
+ 
